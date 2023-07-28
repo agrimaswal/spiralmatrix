94551ebd4a825da6f0b6a3d28123d1a6a9f1b24b
AGRIM ASWAL <124095607+agrimaswal@users.noreply.github.com>
2023-07-28 21:39:22 +0530

    Add files via upload

---
+++ spiralmatrix.java
@@ -0,0 +1,53 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> result = new ArrayList<>();
+
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return result;
+        }
+
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int top = 0;
+        int bottom = rows - 1;
+        int left = 0;
+        int right = cols - 1;
+
+        while (top <= bottom && left <= right) {
+            // Traverse top row
+            for (int i = left; i <= right; i++) {
+                result.add(matrix[top][i]);
+            }
+            top++;
+
+            // Traverse rightmost column
+            for (int i = top; i <= bottom; i++) {
+                result.add(matrix[i][right]);
+            }
+            right--;
+
+            // Check if there are more rows to traverse
+            if (top <= bottom) {
+                // Traverse bottom row
+                for (int i = right; i >= left; i--) {
+                    result.add(matrix[bottom][i]);
+                }
+                bottom--;
+            }
+
+            // Check if there are more columns to traverse
+            if (left <= right) {
+                // Traverse leftmost column
+                for (int i = bottom; i >= top; i--) {
+                    result.add(matrix[i][left]);
+                }
+                left++;
+            }
+        }
+
+        return result;
+    }
+}
